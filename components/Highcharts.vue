3f3a539dbfebcf1d37c452a0183376f68e23c032
Taiga Ito <t.ito@sonicmoov.com>
2022-05-05 02:13:26 +0900

    feat: 選択した都道府県データをチャート反映

---
+++ components/Highcharts.vue
@@ -0,0 +1,52 @@
+<template>
+  <highcharts :options="chartOptions" />
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    chartTitle: {
+      type: String,
+      required: true,
+    },
+    axis: {
+      type: Object,
+      required: true,
+    },
+    series: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const chartOptions = computed(() => {
+      return {
+        chart: {},
+        title: {
+          text: props.chartTitle,
+          style: {
+            color: '#333',
+          },
+        },
+        yAxis: {
+          title: {
+            text: props.axis.y.title,
+          },
+        },
+        xAxis: {
+          title: {
+            text: props.axis.x.title,
+          },
+          categories: props.axis.x.categories,
+        },
+        plotOptions: {},
+        series: props.series,
+      }
+    })
+    return { chartOptions }
+  },
+})
+</script>
